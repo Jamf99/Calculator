eda6341eff092b10236da2f072fb46510bea705a
Jamf99 <jamf_99@hotmail.com>
2018-09-03 09:27:54 -0500

    This exception is not needed anymore

--- Calculator/src/main/java/exceptions/IsNotIntegerException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class IsNotIntegerException extends Exception{
-
-	public IsNotIntegerException() {
-		super("Some value of the rational number hasn´t exact root");
-	}
-
-}
